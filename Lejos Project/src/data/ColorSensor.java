30ce42663ba42b5a27f7070dac9fa23a44fbf2f5
jamivih <jamivihanto@outlook.com>
2023-03-28 17:36:25 +0300

    Merge branch 'master' of https://github.com/jamivih/Lejos-project.git

--- Lejos Project/src/data/ColorSensor.java
+++ Lejos Project/src/data/ColorSensor.java
@@ -1,9 +1,6 @@
 package data;
-<<<<<<< HEAD
-=======
 
 import lejos.hardware.Sound;
->>>>>>> branch 'master' of https://github.com/jamivih/Lejos-project.git
 import lejos.hardware.port.SensorPort;
 import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
@@ -20,14 +17,8 @@ public class ColorSensor extends Thread {
         SampleProvider redMode = colorSensor.getRedMode();
 
         // define the black and white threshold values
-<<<<<<< HEAD
-        int blackThreshold = 10;
-        int whiteThreshold = 50;
-        
-=======
         int blackThreshold = 5;
         int whiteThreshold = 70;
->>>>>>> branch 'master' of https://github.com/jamivih/Lejos-project.git
 
         // continuously read and print the detected color
         while (true) {
@@ -35,28 +26,11 @@ public class ColorSensor extends Thread {
             redMode.fetchSample(redSample, 0);
             float redValue = redSample[0];
             if (redValue < blackThreshold) {
-<<<<<<< HEAD
-                System.out.println("Black detected");
-=======
                 Sound.beep();
->>>>>>> branch 'master' of https://github.com/jamivih/Lejos-project.git
             } else if (redValue > whiteThreshold) {
-                System.out.println("White detected");
+                Sound.buzz();
             }
-<<<<<<< HEAD
-            Delay.msDelay(500);
-=======
             Delay.msDelay(500); // add delay between value checks
->>>>>>> branch 'master' of https://github.com/jamivih/Lejos-project.git
         }
     }
-<<<<<<< HEAD
-
-    public static void main(String[] args) {
-        // create a new instance of the ColorSensor thread and start it
-        ColorSensor colorSensor = new ColorSensor();
-        colorSensor.start();
-    }
-=======
->>>>>>> branch 'master' of https://github.com/jamivih/Lejos-project.git
 }
