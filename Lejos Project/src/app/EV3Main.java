9057053dce8be3df6e5d2c24c398934ddd8d0677
jamivih <jamivihanto@outlook.com>
2023-03-28 20:20:17 +0300

    updates

--- Lejos Project/src/app/EV3Main.java
+++ Lejos Project/src/app/EV3Main.java
@@ -6,10 +6,10 @@ public class EV3Main {
 
 	public static void main(String[] args) {
 		
-//		UltrasonicSensor obd = new UltrasonicSensor(null);	
+		UltrasonicSensor obd = new UltrasonicSensor(null);	
 		ColorSensor colorSensor = new ColorSensor();
 		
-//		obd.start();
+		obd.start();
 		
         colorSensor.start();
 
