227803f02f28d07a61268663ac7c3299a6302a2a
jamivih <jamivihanto@outlook.com>
2023-04-04 14:29:34 +0300

    error fix

--- Lejos Project/src/data/ColorSensor.java
+++ Lejos Project/src/data/ColorSensor.java
@@ -1,28 +1,30 @@
 package data;
 
 import java.io.File;
-
 import lejos.hardware.Button;
 import lejos.hardware.Sound;
 import lejos.hardware.motor.Motor;
 import lejos.hardware.port.SensorPort;
+import lejos.robotics.Color;
 import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 
 public class ColorSensor extends Thread {
-	DataExchange DEObj;
+    DataExchange DEObj;
+    private EV3ColorSensor colorSensor;
+    private EV3UltrasonicSensor us;
+    private MotorController motorController = new MotorController();
+    
 
-	private EV3ColorSensor colorSensor;
-	private EV3UltrasonicSensor us;
-
-	
-	public ColorSensor(DataExchange DE) {
-		DEObj = DE;
-		colorSensor = new EV3ColorSensor(SensorPort.S4);
-//		us = new EV3UltrasonicSensor(SensorPort.S1);
-	}
+    public ColorSensor(DataExchange DE) {
+        DEObj = DE;
+        colorSensor = new EV3ColorSensor(SensorPort.S3);
+        
+        
+//      us = new EV3UltrasonicSensor(SensorPort.S1);
+    }
     @Override
     public void run() {
         // create a color sensor object on port 4      
@@ -31,49 +33,37 @@ public class ColorSensor extends Thread {
 
         // define the black and white threshold values
         float blackThreshold = 0.15f;
-        float whiteThreshold = 0.55f;
+        float whiteThreshold = 0.30f;
+        
         
         
         // continuously read and print the detected color
-        while (Button.ESCAPE.isUp()) {
-        	float[] redSample = new float[redMode.sampleSize()];
+        while (true) {
+            float[] redSample = new float[redMode.sampleSize()];
             redMode.fetchSample(redSample, 0);
-            float redValue = redSample[0];         
-        	if (DEObj.getCMD() == 1) {              
-	            if (blackThreshold < redValue && redValue <= whiteThreshold) {
-	            	Motor.A.setSpeed(100);
-	            	Motor.B.setSpeed(100);
-	            	Motor.A.forward();
-	            	Motor.B.forward();
-	            } else if (redValue < whiteThreshold) {
-	            	Motor.A.setSpeed(100);
-	            	Motor.B.setSpeed(30);
-	            	Motor.A.forward();
-	            	Motor.B.forward();
-	            } else {
-	            	Motor.A.setSpeed(30);
-	            	Motor.B.setSpeed(100);
-	            	Motor.A.forward();            	
-	            	Motor.B.forward();
-	            }
-        	} else {            	
-            	Motor.A.setSpeed(100);
-            	Motor.B.setSpeed(100);
-            	Motor.A.backward();            	
-            	Motor.B.backward();
-            	Delay.msDelay(2500);
-            	
-            	Motor.B.stop();
-            	Motor.A.stop();
-            	Sound.playSample(new File("haista.wav"), Sound.VOL_MAX);
-            	
-            	Motor.A.setSpeed(500);
-            	Motor.B.setSpeed(500);
-            	Motor.A.forward();
-            	Motor.B.forward();
-            	Delay.msDelay(3000);
-            	
-	            }
-        	}
+            float redValue = redSample[0];
+            
+
+
+            if (DEObj.getCMD() == 1) {      
+
+                if (blackThreshold <= redValue && redValue <= whiteThreshold) {
+                    motorController.moveForward(100);
+                } else
+                	if (redValue < whiteThreshold) {
+                    motorController.turnRight(175, 25);
+                } else {
+                    motorController.turnLeft(25, 175);
+                }
+                
+                
+            } 
+            
+            if (Button.ESCAPE.isDown()) {
+                // exit program
+                System.exit(0);
+            }
+
         }
     }
+}
--- Lejos Project/src/data/ObstacleDetector.java
+++ Lejos Project/src/data/ObstacleDetector.java
@@ -1,42 +1,44 @@
 package data;
 
 import lejos.hardware.Button;
-import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.SampleProvider;
 
-public class ObstacleDetector extends Thread{
-	private DataExchange DEObj;
-	
-	private EV3UltrasonicSensor us;
-	private final int securityDistance = 13;
+public class ObstacleDetector extends Thread {
+    private DataExchange DEObj;
+    private EV3UltrasonicSensor us;
+    private final int securityDistance = 10;
+    private MotorController motorController = new MotorController();
 
-	public ObstacleDetector(DataExchange DE){
-		DEObj = DE;
-		us = new EV3UltrasonicSensor(SensorPort.S1);
-		
-	}
-	public void run() {		
-		SampleProvider sp = us.getDistanceMode();
-		float[] distanceSample = new float[sp.sampleSize()];
-		
-	while (Button.ESCAPE.isUp()) {	
-		sp.fetchSample(distanceSample, 0);
-		float distance = distanceSample[0] * 100;
-		if (distance > securityDistance) {
-			DEObj.setCMD(1);
+    public ObstacleDetector(DataExchange DE) {
+        DEObj = DE;
+        us = new EV3UltrasonicSensor(SensorPort.S1);
+    }
 
-		} else {
-			
-			DEObj.setCMD(0);
-			
-			}
-		
-		
-		}
-	
-	}
-	
+    public void run() {
+        SampleProvider sp = us.getDistanceMode();
+        float[] distanceSample = new float[sp.sampleSize()];
+
+        while (true) {
+            sp.fetchSample(distanceSample, 0);
+            float distance = distanceSample[0] * 100;
+            if (distance > securityDistance) {
+                DEObj.setCMD(1);
+            } else {
+                DEObj.setCMD(0);
+                LCD.drawString("Avoid Obstacle", 0, 0);
+                LCD.refresh();
+                motorController.performObstacleAvoidance();
+                DEObj.setCMD(1);
+                LCD.clear();
+            }
+            
+            if (Button.ESCAPE.isDown()) {
+                // exit program
+                System.exit(0);
+            }
+        }
+    }
 }
