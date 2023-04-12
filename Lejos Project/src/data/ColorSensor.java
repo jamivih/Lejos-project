13093514548a5e32cbd5c2c13a8f4a870d74b9ae
jamivih <jamivihanto@outlook.com>
2023-04-12 14:18:10 +0300

    colorsensor white and black updates

--- Lejos Project/src/data/ColorSensor.java
+++ Lejos Project/src/data/ColorSensor.java
@@ -9,6 +9,7 @@ import lejos.robotics.Color;
 import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 import lejos.hardware.sensor.EV3ColorSensor;
+<<<<<<< Updated upstream
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 
 public class ColorSensor extends Thread {
@@ -18,51 +19,59 @@ public class ColorSensor extends Thread {
     private MotorController motorController = new MotorController();
     
     private long lastLineTime = 0;
+=======
 
-    public ColorSensor(DataExchange DE) {
-        DEObj = DE;
-        colorSensor = new EV3ColorSensor(SensorPort.S3);
-    }
-    
-    @Override
-    public void run() {
-        // create a color sensor object on port 4      
-        // get the red color mode sample provider
-        SampleProvider redMode = colorSensor.getRedMode();
+public class ColorSensor extends Thread {
+	DataExchange DEObj;
+	private EV3ColorSensor colorSensor;
+//    private EV3UltrasonicSensor us;
+	private MotorController motorController = new MotorController();
+>>>>>>> Stashed changes
 
-        // define the black and white threshold values
-        float blackThreshold = 0.15f;
-        float whiteThreshold = 0.25f;
-    
-        long timeThreshold = 1000;
-        
-        
+	private long lastLineTime = 0;
 
-        while (true) {
-            float[] redSample = new float[redMode.sampleSize()];
-            redMode.fetchSample(redSample, 0);
-            float redValue = redSample[0];
-            
-            if (DEObj.getCMD() == 1)
-            
+	public ColorSensor(DataExchange DE) {
+		DEObj = DE;
+		colorSensor = new EV3ColorSensor(SensorPort.S3);
+	}
 
-            if (blackThreshold <= redValue && redValue <= whiteThreshold) {
-                motorController.moveForward(100);
-                lastLineTime = System.currentTimeMillis();
-            } else {
+	@Override
+	public void run() {
+		// create a color sensor object on port 4
+		// get the red color mode sample provider
+		SampleProvider redMode = colorSensor.getRedMode();
 
-                if (System.currentTimeMillis() - lastLineTime > timeThreshold) {
-                    motorController.turnRight(175, 25);
-                } else if (redValue < whiteThreshold) {
-                    motorController.turnRight(175, 25);
-                } else {
-                    motorController.turnLeft(25, 175);
-                }
-            }
-            
-            if (Button.ESCAPE.isDown()) {
-                System.exit(0);
-            }
-        }
-    }
+		// define the black and white threshold values
+		float blackThreshold = 0.10f;
+		float whiteThreshold = 0.40f;
+
+		long timeThreshold = 1000;
+
+		while (true) {
+			float[] redSample = new float[redMode.sampleSize()];
+			redMode.fetchSample(redSample, 0);
+			float redValue = redSample[0];
+
+			if (DEObj.getCMD() == 1)
+
+				if (blackThreshold <= redValue && redValue <= whiteThreshold) {
+					motorController.moveForward(100);
+					lastLineTime = System.currentTimeMillis();
+				} else {
+
+					if (System.currentTimeMillis() - lastLineTime > timeThreshold) {
+						motorController.turnRight(175, 25);
+						if (redValue < whiteThreshold) {
+							motorController.turnRight(175, 25);
+						} else {
+							motorController.turnLeft(25, 175);
+						}
+					}
+
+					if (Button.ESCAPE.isDown()) {
+						System.exit(0);
+					}
+				}
+		}
+	}
 }
